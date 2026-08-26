baed88b2450aea369466eb34f2a8bb3543f67823
pranavmaheshwari15 <pranav.maheshwari15@gmail.com>
2026-08-26 18:06:26 +0530

    Defining region of interest

--- Task2/main.py
+++ Task2/main.py
@@ -6,9 +6,16 @@ def canny(image):
     canny = cv2.Canny(blur,50,150)      #Traces edges which correspond to maximum change in brightness
     return canny
 
+def region_of_interest (image):
+    height = image.shape[0]
+    polygons = np.array([[(200,height),(1100,height),(550,250)]])
+    mask = np.zeros_like(image)     #Create ana rray of zeroes with same shape as image
+    cv2.fillPoly(mask,polygons,255)     #To fill the color white
+    return mask
+
 image = cv2.imread("Input/1.png")  #To load the image
 lane_image = np.copy(image)     #Copying the array of image
 canny = canny(lane_image)
 
-cv2.imshow('result',canny)  #To read image
+cv2.imshow('result',region_of_interest(canny))  #To read image
 cv2.waitKey(0)   #Display result window until we press anything on keyboard
